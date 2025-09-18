072fc748f3e0b70ac18c6434a5285394baba57cf
Atsilah Amany <atsilahamanyph@gmail.com>
2025-09-19 06:30:05 +0700

    PRJ1:  Membuat variabel

--- project1atsilah/bin/project1atsilah.dart
+++ project1atsilah/bin/project1atsilah.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project1atsilah/project1atsilah.dart' as project1atsilah;
 
 const String nama = "atsilah"; // compile time constant
@@ -12,4 +14,21 @@ void main(List<String> arguments) {
   // constanta dan final
   final String nama2 = "amany"; // run time constant  
   print("Nama saya $nama $nama2");
+
+  // varianbel
+  int umur = 20;
+  double tinggi = 160;
+  String alamat = "jl. simpang remujung";
+  bool jomblo = true;
+  bool jenisKelamin = false; // false = perempuan, true = laki-laki
+  stdout.writeln("umur saya $umur tahun");
+  stdout.writeln("tinggi saya $tinggi cm");
+  stdout.writeln("alamat saya di $alamat");
+  stdout.writeln("apakah saya jomblo? $jomblo");
+  stdout.writeln("Jenis kelamin saya ${jenisKelamin ? 'laki-laki' : 'perempuan'}");
+  // if (jenisKelamin == true) {
+  //   stdout.writeln("Jenis kelamin saya laki-laki");
+  // } else {
+  //   stdout.writeln("Jenis kelamin saya perempuan");
+  // } 
 }
