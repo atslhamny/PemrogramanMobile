53cd0ce14d700d8b3a1ebddb3bef23ec2f32bf5d
Atsilah Amany <atsilahamanyph@gmail.com>
2025-09-19 06:10:58 +0700

    PRJ1: Belajar membuat komentar

---
+++ project1atsilah/bin/project1atsilah.dart
@@ -0,0 +1,10 @@
+import 'package:project1atsilah/project1atsilah.dart' as project1atsilah;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project1atsilah.calculate()}!');
+  //  satu line comment
+  /*
+  multi line comment
+  banyak line
+  */
+}
---
+++ project1atsilah/test/project1atsilah_test.dart
@@ -0,0 +1,8 @@
+import 'package:project1atsilah/project1atsilah.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
