ca7b14b676a8f28bb230d05d13fe7ea66be49ea9
Atsilah Amany <atsilahamanyph@gmail.com>
2025-09-19 06:18:02 +0700

    PRJ1: Membuat constanta/final

--- project1atsilah/bin/project1atsilah.dart
+++ project1atsilah/bin/project1atsilah.dart
@@ -1,5 +1,7 @@
 import 'package:project1atsilah/project1atsilah.dart' as project1atsilah;
 
+const String nama = "atsilah"; // compile time constant
+
 void main(List<String> arguments) {
   // print('Hello world: ${project1atsilah.calculate()}!');
   //  satu line comment
@@ -7,4 +9,7 @@ void main(List<String> arguments) {
   multi line comment
   banyak line
   */
+  // constanta dan final
+  final String nama2 = "amany"; // run time constant  
+  print("Nama saya $nama $nama2");
 }
