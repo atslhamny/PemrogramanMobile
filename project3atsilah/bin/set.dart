7ee7ae90ad1dc9c970791378a1b87fd6bb3ed7f1
Atsilah Amany <atsilahamanyph@gmail.com>
2025-09-19 14:02:00 +0700

    PRJ3: Membuat set versi 2

--- project3atsilah/bin/set.dart
+++ project3atsilah/bin/set.dart
@@ -1,18 +1,20 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  var setnilai1 = <int>{};
-  var setnilai2 = <int>{1, 2, 3};
+  var setnilai1 = <String>{};
+  // var setnilai2 = <int>{1, 2, 3};
   stdout.writeln("Masukkan Jumlah Nilai: ");
   String? input = stdin.readLineSync();
   int jumlah1 = int.tryParse(input ?? '') ?? 0;
   for (var i = 0; i < jumlah1; i++) {
     stdout.writeln("Masukkan Nilai ke-${i + 1}: ");
     String? inputnilai = stdin.readLineSync();
-    int nilai = int.tryParse(inputnilai ?? '') ?? 0;
+    String nilai = inputnilai ?? '';
     setnilai1.add(nilai);
   }
-  print("Set Nilai 1: $setnilai1");
-  print(setnilai1.union(setnilai2));
-  print(setnilai1.intersection(setnilai2));
+
+  var nilaitolist = setnilai1.toList();
+  print(nilaitolist.elementAt(0));
+  // print(setnilai1.union(setnilai2));
+  // print(setnilai1.intersection(setnilai2));
 }
