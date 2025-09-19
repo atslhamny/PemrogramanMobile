c4f1b3d22bc8c79e0d407f6d89edd8458ec6d4ce
Atsilah Amany <atsilahamanyph@gmail.com>
2025-09-19 11:21:45 +0700

    PRJ3: Membuat Fixed List

--- project3atsilah/bin/project3atsilah.dart
+++ project3atsilah/bin/project3atsilah.dart
@@ -1,5 +1,19 @@
+import 'dart:io';
+
 import 'package:project3atsilah/project3atsilah.dart' as project3atsilah;
 
 void main(List<String> arguments) {
   print('Hello world: ${project3atsilah.calculate()}!');
+
+var fl = List<int>.filled(4,12);
+  fl[0] = 10;
+  fl[1] = 20;
+  fl[2] = 30;
+  fl[3] = 40;
+  // fl[4] = 50;
+
+  //Cetak List
+  stdout.writeln(fl);
+
+  
 }
