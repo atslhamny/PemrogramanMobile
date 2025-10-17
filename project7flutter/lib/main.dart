7218be9dba6572affa2e52c1379d15db30f31e4a
Atsilah Amany <atsilahamanyph@gmail.com>
2025-10-17 10:09:50 +0700

    PRJ7: Bagian 2 Membangun Layout

--- project7flutter/lib/main.dart
+++ project7flutter/lib/main.dart
@@ -57,41 +57,73 @@ import 'package:flutter/material.dart';
 //   }
 // }
 
+// void main() {
+//   runApp(StackExample());
+// }
+
+// class StackExample extends StatelessWidget {
+//   const StackExample({super.key}); // Tambahkan konstruktor dengan super.key
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Stack Example'),
+//         ),
+//         body: Center(
+//           child: Stack(
+//             alignment: Alignment.center,
+//             children: [
+//               Container(
+//                 width: 200,
+//                 height: 200,
+//                 color: Colors.blue,
+//               ),
+//               Container(
+//                 width: 150,
+//                 height: 150,
+//                 color: Colors.red,
+//               ),
+//               Text(
+//                 'Tumpang Tindih',
+//                 style: TextStyle(color: Colors.white, fontSize: 20),
+//              )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 void main() {
-  runApp(StackExample());
+  runApp(ProfileLayout());
 }
 
-class StackExample extends StatelessWidget {
-  const StackExample({super.key}); // Tambahkan konstruktor dengan super.key
-
+class ProfileLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Stack Example'),
+          title: Text('Profile Layout'),
         ),
-        body: Center(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 150,
-                height: 150,
-                color: Colors.red,
-              ),
-              Text(
-                'Tumpang Tindih',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-             )
-            ],
+       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50, backgroundImage: AssetImage(  'assets/images/foto.png'),
           ),
-        ),
+          SizedBox(height: 10),
+          Text(
+            'Atsilah Amany Putri Harsuma',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5),
+          Text('Flutter Developer', style: TextStyle(fontSize: 16, color: Colors.grey[600])),
+        ],
+       ),
       ),
     );
   }
