9e84dc11abc1e2b6fe3ef36d3abfa4e973337863
Atsilah Amany <atsilahamanyph@gmail.com>
2025-10-17 09:59:14 +0700

    PRJ7: add Column Example(Multi-child Layout)

--- project7flutter/lib/main.dart
+++ project7flutter/lib/main.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(SingleChildExample());
+// }
+
+// class SingleChildExample extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Single Child Example'),
+//           backgroundColor: Colors.teal,
+//         ),
+//         body: Center(
+//           child: Text(
+//             'Hello, Flutter!',
+//             style: TextStyle(fontSize: 24, color: Colors.teal),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 void main() {
-  runApp(SingleChildExample());
+  runApp(ColumnExample());
 }
 
-class SingleChildExample extends StatelessWidget {
+class ColumnExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Single Child Example'),
+          title: Text('Column Example'),
           backgroundColor: Colors.teal,
         ),
-        body: Center(
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24, color: Colors.teal),
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'ini baris pertama',
+            ),
+            Text(
+              'ini baris kedua',
+            ),
+           ElevatedButton(
+              onPressed: () {},
+              child: Text('TOMBOL INI'),
+            ),
+          ],
         ),
       ),
     );
